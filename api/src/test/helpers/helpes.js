72dd02dcf80daeb5867aac90e143cc5eaf8716b7
Piwexx <vigarioignacio@gmail.com>
2024-05-14 19:47:17 -0300

    test user

--- api/src/test/helpers/helpes.js
+++ api/src/test/helpers/helpes.js
@@ -1,54 +1,9 @@
-const {app,server} = require('../../../app')
-const userRepository = require('../../repositories/userRepository');
-const supertest = require('supertest')
-const api = supertest(app)
+//const {app,server} = require('../../../app')
+//const supertest = require('supertest')
+
+const mockUser = {username: 'Juan@gmail.com', password: 'AAA1234',name:"Juan"};
+const mockToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQSflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
 
 
-//Notes
-const notes = [{
-    title:"Curso CSS",
-    body: "Curso CSS",
-    user:"66219bd69bebb26c7253af32"
-    },
-    {
-    title:"Curso HTML",
-    body: "Curso HTML",
-    user:"66219bd69bebb26c7253af32"
-    }
- ]
 
-  const deleteAll = async()=>{
-    await Note.deleteMany({})
-  }
-
-  const saveNote = async (note)=>{
-    const noteUno = new Note(note)
-    await noteUno.save()
-  }
-
-
- const getAllContentNotes = async() => {
-    const response = await api.get('/notes/all')
-    const contents = response.body.map(note => note.body)
-
-    return {response,contents}
-
- }
-
- //User
-
- const deleteAllUsers = async()=>{
-  await  await userRepository.deleteAll()
-}
-
-const saveUser = async (username,name,password)=>{
-  const user = userRepository.createUser({username,password,name})
-  return user
-}
- 
-const getAllUsers = async()=>{
-  const users = await userRepository.getAll()
-  return users
-}
-
-module.exports = {app,server,deleteAllUsers,saveUser,getAllUsers}
+module.exports = {mockUser,mockToken}
